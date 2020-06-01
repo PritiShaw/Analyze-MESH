4ffdcb6e432e6b84acbc258e8c7091c8aab431ff
Priti Shaw <pritishaw0103@gmail.com>
2020-06-02 02:17:10 +0530

    Add topic_term column

--- CompareMeshCSV.py
+++ CompareMeshCSV.py
@@ -2,12 +2,23 @@ import csv
 from urllib.request import urlopen
 from xml.etree.ElementTree import parse
 
+filenames = ["Aging/pmid-AgingMeSHT-set.txt", "CommunicableDiseases/pmid-Communicab-set.txt",
+ "ComputationalBiology/pmid-Computatio-set.txt", "Neoplasms/pmid-NeoplasmsM-set.txt", "Neurosciences/pmid-neurosciene-set.txt"]
+def search(pmid):
+    for filename in filenames:
+        with open(filename) as infile:
+            datafile = infile.readlines()
+            for line in datafile:
+                if int(pmid)==int(line):
+                    return(filename.split('/')[0])
+
 f = open("AllPMID.txt", "r")
 Dictonary ={}
 for i in f:
     var_url = urlopen('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&id={}'.format(i))
     i= str(i.strip())
-    Dictonary[i] = {'eutil':[],'webapi':[]}
+    Dictonary[i] = {'eutil':[],'webapi':[], 'topic_term':[]}
+    Dictonary[i]['topic_term'].append((search(i)))
     xmldoc = parse(var_url)
     for item in xmldoc.iterfind('PubmedArticle/MedlineCitation/MeshHeadingList'):
         for e in item.findall('MeshHeading'):
@@ -15,7 +26,7 @@ for i in f:
                 Dictonary[i]['eutil'].append(e.find('DescriptorName').text)
     #print(Dict)
 f.close()
-print("STEP1 Completed...")
+#print("STEP1 Completed...")
 
 readfile = open('AllMeshTerms.txt', "r")
 for line in readfile:
@@ -28,21 +39,23 @@ for line in readfile:
         #print(PMID,MESH)
         Dictonary[pmid]['webapi'].append(mesh)
 #print(Dictonary)
-print("STEP2 Completed...")
+#print("STEP2 Completed...")
 
 
 with open('compare.tsv', mode='w') as f:
     writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    print("PMID","EUTIL","WEBAPI",sep='\t',file=f)
+    print("PMID","EUTIL","WEBAPI", "TOPIC TERM",sep='\t',file=f)
     for key,val in Dictonary.items():
         list_A= val['eutil']
         list_B= val['webapi']
+        list_C= val['topic_term']
         len_A=len(list_A)
         len_B=len(list_B)
         max_length= max(len_A,len_B)
         for idx in range(max_length):
-            row = [key,"",""]
+            row = [key,"","",""]
             row[1] = list_A[idx] if idx<len_A else "NA"
             row[2] = list_B[idx] if idx<len_B else "NA"
+            row[3] = list_C[0] if idx<1 else list_C[0]
             print('\t'.join(row),file=f)
-print("STEP3 Completed...")
+#print("STEP3 Completed...")
