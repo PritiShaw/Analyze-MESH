a40f42068b2b8feb5730810b6a376f8e765e1409
Priti Shaw <pritishaw0103@gmail.com>
2020-11-19 13:54:25 +0530

    Handle authentication using API key

--- example.py
+++ example.py
@@ -19,27 +19,41 @@ def remove_non_ascii(filepath, temp_file):
     uc.close()
     op.close()
 
+def getInput(msg, err = "Please enter valid input"):
+    res = ""
+    while True:
+        res = getpass.getpass(msg)
+        if len(res)>0:
+            return res
+        else:
+            print(err)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
 
     parser.add_argument('file', help="Path to input file")
     parser.add_argument('email', help="Email ID for identification by MTI")
-    parser.add_argument('user', help="MTI Username")
 
     parser.add_argument(
-        '-output', help="Output file path, Default: result.tsv", default="result.tsv")
+        '--user', '-u', help="MTI Username, if not provided, API key will be required", default=None)
+    parser.add_argument(
+        '--output', help="Output file path, Default: result.tsv", default="result.tsv")
 
     args = parser.parse_args()
 
-    password = getpass.getpass("Enter MTI password > ")
     temp_file = ".tmp_mti_process_non_ascii.txt"
-
     remove_non_ascii(args.file, temp_file)
 
+    if args.user:
+        password = getInput("Enter MTI password > ", "Please Enter valid Password")
+        processed_result = handleMTIRequest(
+            temp_file, args.email, ["--singleLinePMID"], username=args.user, password=password)
+    else:
+        apikey = getInput("Enter MTI APIKey > ", "Please Enter valid API-Key")
+        processed_result = handleMTIRequest(
+            temp_file, args.email, ["--singleLinePMID"], apikey=apikey)
+
     # processed_result is a Dictionary
-    processed_result = handleMTIRequest(
-        temp_file, args.email, args.user, password, ["--singleLinePMID"])
 
     # Handle output format
     with open(args.output, "w+") as res_op:
